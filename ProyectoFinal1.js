4d30bd793bcde5773378e5d0dd6f50854910d392
Ivan Falcon <ivanfalcon52@gmail.com>
2023-08-19 18:29:47 -0300

    guardar cambios segunda entrega proyecto final

--- ProyectoFinal1.js
+++ ProyectoFinal1.js
@@ -1,33 +1,24 @@
-alert("Bienvenido al programa para calcular el valor de las cuotas a abonar de su producto");
+alert("Bienvenido al programa para calcular el valor de cada cuota a abonar del producto ofrecido en 12 cuotas sin interes");
 
-let producto = prompt ("ingrese el producto: ");
+const productos = [
+    {nombre: "playstation 5", precio: 580000},
+    {nombre: "playstation 4", precio: 200000},
+    {nombre: "xbox one", precio: 250000},
+    {nombre: "pc gamer", precio: 300000},
+];
 
-if (producto === "playstation 5") {
-    producto = 580000;
-} else if (producto === "playstation 4") {
-    producto = 200000;
-} else if (producto === "xbox one") {
-    producto = 250000;
-} else if (producto === "pc gamer") {
-    producto = 300000;
-} else {
-    alert ("el valor ingresado no es valido")
-    producto = prompt ("ingrese el producto")
-}
+let nombre = prompt ("ingrese el producto: ");
 
-const DoceSinInteres = x => x / 12;
+while (nombre != "ESC") {
+    const producto = productos.find((item) => item.nombre === nombre);
 
-let valorCuota= DoceSinInteres (producto);
-alert (`El valor de cada cuota del producto es: ${valorCuota}`);
-
-
-
-
-
-
-
-
-
-
-
+    if (producto) {
+        let valorCuota = producto.precio/12;
+        valorCuota = valorCuota.toFixed(2);
+        alert (`El valor de cada cuota del producto es: ${valorCuota}`)
+    } else {
+            alert("producto no encontrado")
+        }
+        nombre = prompt ("ingrese el producto: "); 
+}   
 
